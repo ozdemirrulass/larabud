c8ca67e99fb95e2c88d9c11899ec2a39eef6d75c
Ulaş Özdemir <ozdemirrulass@gmail.com>
2024-10-17 05:50:23 +0300

    nest: add github config

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -7,12 +7,14 @@ import { JwtModule, JwtService } from '@nestjs/jwt';
 import jwtConfig from './config/jwt.config';
 import { ConfigModule } from '@nestjs/config';
 import refreshConfig from './config/refresh.config';
+import githubOauthConfig from './config/github-oauth.config';
 
 @Module({
   imports: [
     JwtModule.registerAsync(jwtConfig.asProvider()),
     ConfigModule.forFeature(jwtConfig),
     ConfigModule.forFeature(refreshConfig),
+    ConfigModule.forFeature(githubOauthConfig),
   ],
   controllers: [AuthController],
   providers: [AuthService, UserService, PrismaService, JwtService],
