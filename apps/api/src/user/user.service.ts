a309b7a3c9c31502ba448adaaafc0acb7b41a2bf
Ulaş Özdemir <ozdemirrulass@gmail.com>
2024-10-17 03:41:21 +0300

    nest: register user - service & controller initial structure

--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { hash } from 'argon2';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private readonly prisma: PrismaService) { }
+    async create(createUserDto: RegisterUserDto) {
+        const { password, ...user } = createUserDto;
+        const hashedPassword = await hash(password);
+        return await this.prisma.users.create({
+            data: {
+                password: hashedPassword,
+                ...user
+            }
+        })
+    }
+
+    async findByEmail(email: string) {
+        return await this.prisma.users.findUnique({
+            where: {
+                email,
+            }
+        })
+    }
+}
