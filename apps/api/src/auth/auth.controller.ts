6354a1cb0b13271cd3687ede33eff803304566b0
Ulaş Özdemir <ozdemirrulass@gmail.com>
2024-10-17 03:47:09 +0300

    nest: auth controller - initial register method

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterUserDto } from 'src/user/dto/register-user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
+  @Post("register")
+  registerUser(@Body() registerUserDto: RegisterUserDto) {
+    return this.authService.registerUser(registerUserDto)
+  }
 }
