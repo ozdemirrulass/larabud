45de767e2b1ede745f40d04547bbef0eb46e2d9e
Ulaş Özdemir <ozdemirrulass@gmail.com>
2024-10-17 03:45:05 +0300

    nest: auth service - initial register method

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { RegisterUserDto } from 'src/user/dto/register-user.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly userService: UserService,
+    ) { }
+
+    async registerUser(registerUserDto: RegisterUserDto) {
+        const user = await this.userService.findByEmail(registerUserDto.email);
+        if (user) {
+            throw new ConflictException("User already exists!")
+        }
+        return this.userService.create(registerUserDto)
+    }
+}
