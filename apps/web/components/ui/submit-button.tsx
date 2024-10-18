c350c45f05990a82b7115db53ad3b86e68a6f3c5
Ulaş Özdemir <ozdemirrulass@gmail.com>
2024-10-18 23:36:17 +0300

    next: add reusable submit button

---
+++ apps/web/components/ui/submit-button.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { Button } from '@repo/ui/components/ui/button'
+import React, { PropsWithChildren } from 'react'
+import { useFormStatus } from 'react-dom'
+
+const SubmitButton = ({ children }: PropsWithChildren) => {
+    const { pending } = useFormStatus()
+    return (
+        <Button type='submit' aria-disabled={pending} className='w-full mt-2'>
+            {pending ? "Submitting..." : children}
+        </Button>
+    )
+}
+
+export default SubmitButton
