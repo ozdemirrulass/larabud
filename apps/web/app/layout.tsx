db899a4584effca1b4ba79a0659e7c65e5a79a4f
Ulaş Özdemir <ozdemirrulass@gmail.com>
2024-10-20 00:53:32 +0300

    next: provider rev

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "@repo/ui/globals.css"
-import TopBar from "@/components/layout/top-bar";
-import { ThemeProvider } from "@/components/layout/theme-provider";
+import { Providers } from "@/components/providers/providers";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,16 +29,9 @@ export default function RootLayout({
           backgroundImage: 'url(https://pinkary.com/img/dots.svg)',
           backgroundSize: '550% 550%'
         }}>
-
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
+        <Providers>
           {children}
-        </ThemeProvider>
-
+        </Providers>
       </body>
     </html>
   );
